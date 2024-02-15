dae87315750e5570e19821fc896d56432439213b
Gioshox <luukas.saarinen@gmail.com>
2024-02-15 16:39:11 +0200

    Layout update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -61,6 +61,63 @@
             </aside>
             <div class="content">
                 <h1>Uusi Skenaario</h1>
+                <div class="content-box">
+                    <h1>Perustiedot</h1>
+                    <form action="">
+
+                    <div class="form-container">
+                        <div class="form-box">
+                            <div>
+                                <div class="form-group">
+                                    <label for="name">Nimi</label>
+                                    <input type="text" id="name" name="name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Kuvaus</label>
+                                    <textarea id="description" name="description" rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="form-group">
+                                    <label for="minlvl">Hahmojen kokemustaso</label>
+                                    <div class="row">
+                                        <input type="number" id="minlvl" name="minlvl" min="1" value="1">
+                                        <span>-</span>
+                                        <input type="number" id="maxlvl" name="maxlvl" min="1" value="1">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="minlvl">Hahmojen määrä</label>
+                                    <div class="row">
+                                        <input type="number" id="minplayers" name="minplayers" min="1" value="1">
+                                        <span>-</span>
+                                        <input type="number" id="maxplayers" name="maxplayers" min="1" value="1">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="requirements">Muut vaatimukset</label>
+                                    <input type="text" id="requirements" name="requirements">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-box">
+                            <div>
+                                <div class="form-group">
+                                    <label for="description">Kuvaus pelinjohtajalle</label>
+                                    <textarea id="description" name="description" rows="5"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Taustatiedot</label>
+                                    <textarea id="description" name="description" rows="5"></textarea>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    </form>
+                </div>
             </div>
         </div>
     </main>
