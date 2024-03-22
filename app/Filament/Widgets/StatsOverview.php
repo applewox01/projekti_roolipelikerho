138f4943ec09fb75e5d60908523cd52df68d0513
Gioshox <123938225+Gioshox@users.noreply.github.com>
2024-03-22 18:25:18 +0200

    Widget päivitys

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\characters;
+use App\Models\Scenario;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -11,8 +13,8 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         $totalUsers = User::count();
-        $totalCharacters = 0;
-        $totalScenarios = 0;
+        $totalCharacters = characters::count();
+        $totalScenarios = Scenario::count();
         return [
             Stat::make('Käyttäjät', $totalUsers),
             Stat::make('Hahmot', $totalCharacters),
