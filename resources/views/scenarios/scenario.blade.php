9f1fb0d7c25f8561ef8eb6c3f3e8fa4536c913a9
omena <joensuuniko1@gmail.com>
2024-04-04 11:45:19 +0300

    removed fontawesome 5

--- resources/views/scenarios/scenario.blade.php
+++ resources/views/scenarios/scenario.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/df37ec336e.js" crossorigin="anonymous"></script>
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/scenarios/scenario-window.css')}}">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
