a43db7e459140965420408da42f90d1b38d3fe1f
Gioshox <123938225+Gioshox@users.noreply.github.com>
2024-05-16 09:39:25 +0300

    fix Class "App\Models\Worlds" not found

--- app/Filament/Resources/WorldsResource.php
+++ app/Filament/Resources/WorldsResource.php
@@ -3,19 +3,16 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\WorldsResource\Pages;
-use App\Filament\Resources\WorldsResource\RelationManagers;
-use App\Models\Worlds;
+use App\Models\worlds;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class WorldsResource extends Resource
 {
-    protected static ?string $model = Worlds::class;
+    protected static ?string $model = worlds::class;
 
     protected ?string $heading = 'Skenaario Maailmat';
     protected ?string $subheading = 'Skenaario Maailmat';
