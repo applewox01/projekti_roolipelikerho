0805aadcb16eec846c9c5eb736a1ea13eb5426ed
Z3R0zz <123938225+Z3R0zz@users.noreply.github.com>
2024-08-08 17:11:36 +0300

    update scenarios page

--- app/Filament/Pages/Scenarios.php
+++ app/Filament/Pages/Scenarios.php
@@ -10,6 +10,7 @@ use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Actions\Action as HeaderAction;
+use Filament\Tables\Actions\ActionGroup;
 
 class Scenarios extends Page implements HasTable
 {
@@ -39,43 +40,68 @@ class Scenarios extends Page implements HasTable
         return $table
             ->query(Scenario::query())
             ->columns([
-                TextColumn::make('name'),
+                TextColumn::make('name')
+                    ->label('Nimi')
+                    ->searchable()
+                    ->sortable(),
                 TextColumn::make('description')
+                    ->label('Kuvaus')
                     ->html()
                     ->words(5),
                 TextColumn::make('background_info')
+                    ->label('Taustatiedot')
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('other_requirements')
+                    ->label('Muut vaatimukset')
                     ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('lvl_highest'),
-                TextColumn::make('lvl_lowest'),
-                TextColumn::make('plr_most'),
-                TextColumn::make('plr_least'),
-                TextColumn::make('world.name'),
+                TextColumn::make('lvl_highest')
+                    ->label('Korkein taso')
+                    ->sortable(),
+                TextColumn::make('lvl_lowest')
+                    ->label('Matalin taso')
+                    ->sortable(),
+                TextColumn::make('plr_most')
+                    ->label('Eniten pelaajia')
+                    ->sortable(),
+                TextColumn::make('plr_least')
+                    ->label('Vähiten pelaajia')
+                    ->sortable(),
+                TextColumn::make('world.name')
+                    ->label('Maailma')
+                    ->searchable()
+                    ->sortable(),
                 TextColumn::make('admin_desc')
+                    ->label('Ylläpitäjän kuvaus')
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('created_at')
+                    ->label('Luotu')
                     ->dateTime()
                     ->since()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('updated_at')
+                    ->label('Päivitetty')
                     ->dateTime()
                     ->since()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->actions([
-                Action::make('delete')
-                    ->requiresConfirmation()
-                    ->action(function (Scenario $scenario) {
-                        $scenario->delete();
-                    })
-                    ->icon('heroicon-o-trash')
-                    ->color('danger'),
-                Action::make('edit')
-                    ->url(fn (Scenario $scenario): string => route('filament.admin.pages.scenario-edit', ['edit' => $scenario]))
-                    ->openUrlInNewTab()
-                    ->icon('gmdi-edit')
-                    ->color('info'),
+                ActionGroup::make([
+                    Action::make('edit')
+                        ->url(fn (Scenario $scenario): string => route('filament.admin.pages.scenario-edit', ['edit' => $scenario]))
+                        ->openUrlInNewTab()
+                        ->name('Muokkaa')
+                        ->icon('heroicon-o-pencil')
+                        ->color('primary'),
+                    Action::make('delete')
+                        ->name('Poista')
+                        ->action(fn (Scenario $record) => $record->delete())
+                        ->requiresConfirmation()
+                        ->modalHeading('Poista skenaario')
+                        ->modalDescription('Haluatko varmasti poistaa skenaarion?')
+                        ->modalSubmitActionLabel('Kyllä, poista skenaario')
+                        ->icon('heroicon-o-trash')
+                        ->color('danger'),
+                ])
             ])
             ->emptyStateHeading('Ei skenaarioita');
     }
